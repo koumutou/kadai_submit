3a01f464cf06ee56fa715594599c3bb48b35d9f7
koumutou <kou.mutou@gmail.com>
2021-12-18 05:56:57 +0900

    memopad_submit

--- js/main.js
+++ js/main.js
@@ -1,95 +1,76 @@
 
-function janken_rock(){
-    let CPU = ["グー","チョキ","パー"];
-    let hand = Math.floor(Math.random() *3);
-    if(hand === 0){
-        let result = "あいこです。もう一回勝負！";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/0.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }else if (hand === 1){
-        let result = "おめでとうございます！勝ちです。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/1.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }else{
-        let result = "残念。負けです。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/2.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }
-}
+/* 入力項目をテーブルへ転記 */
+
+$("#save").on("click", function(){
+    let statusselect = document.getElementById("status");
+    let statusoption = statusselect.selectedIndex;
+    let statustext = statusselect.options[statusoption].text;
+    localStorage.setItem("status",statustext);
+
+    let todotext = $("#todo").val();
+    localStorage.setItem("todo",todotext);
+
+    let deadlinetext = document.getElementById("deadline").value;
+    localStorage.setItem("deadline",deadlinetext);
+
+    let inchargetext = $("#incharge").val();
+    localStorage.setItem("incharge",inchargetext);
+
+    let nextselect = document.getElementById("nextstep");
+    let nextoption = nextselect.selectedIndex;
+    let nexttext = nextselect.options[nextoption].text;
+    localStorage.setItem("nextstep",nexttext);
+
+    let notetext = $("#note").val();
+    localStorage.setItem("note",notetext);
+
+    // let deleteButton = document.createElement('button');
+    // deleteButton.innerHTML = 'Delete';
+    // $("#list").append(deleteButton);
+    // deleteButton.addEventListener('click', evt => {
+    //     evt.preventDefault();
+    //     deleteTasks(deleteButton);
+    // });
+
+    const html = `
+        <tr>
+            <td>${statustext}</td>
+            <td>${todotext}</td>
+            <td>${deadlinetext}</td>
+            <td>${inchargetext}</td>
+            <td>${nexttext}</td>
+            <td>${notetext}</td>
+        </tr>
+        `;
+    $("#list").append(html);
+
+    
+ 
+    
+});
+
+//*　各項目を削除する */ 
+// $("#delete").on("click",function(){
+//     localStorage.clear();
+//     $("#list").empty();
+// });
 
 
+/*更新時のデータ呼び出し→keyが単品でしか作れず*/ 
 
-function janken_scissors(){
-    let CPU = ["グー","チョキ","パー"];
-    let hand = Math.floor(Math.random() *3);
-    if(hand === 0){
-        let result = "残念。負けです。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/0.jpg";       
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }else if (hand === 1){
-        let result = "あいこです。もう一回勝負！";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/1.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }else{
-        let result = "おめでとうございます！勝ちです。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/2.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }
-}
+for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i); 
+    let value = localStorage.getItem(key);
+    const html = `
+            <td>${value}</td>
+        `;
+    $("#list").append(html);
+};
 
 
-
-function janken_paper(){
-    let CPU = ["グー","チョキ","パー"];
-    let hand = Math.floor(Math.random() *3);
-    if(hand === 0){
-        let result = "おめでとうございます！勝ちです。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/0.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#rock_draw").style.color = "red";
-    }else if (hand === 1){
-        let result = "残念。負けです。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/1.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#rock_draw").style.color = "red";
-    }else{
-        let result = "あいこです。もう一回勝負。";
-        document.querySelector("#rock_draw").innerHTML = result;
-        document.querySelector("#CPU_hand").src = "img/2.jpg";
-        document.querySelector("#CPU_hand").style.width = "300px";
-        document.querySelector("#CPU_hand").style.height = "180px";
-        document.querySelector("#CPU_hand").style.margin = "auto";
-        document.querySelector("#rock_draw").style.color = "red";
-    }
-}
-
-
-
+/*削除データが一括選択に*/ 
+$("#list").on("click", function () {
+    const key2 = $(this).children('td').text();
+    localStorage.removeItem(key2);
+    $(this).empty();
+      });
